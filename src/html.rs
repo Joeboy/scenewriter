9c6f4dbf3b5bcc442015919182b92c7c8a64c6ca
Joe Button <j.button@reckondigital.com>
2023-03-13 18:36:51 +0000

    Allow write_html() to take any writable instead of just File

--- src/html.rs
+++ src/html.rs
@@ -1,6 +1,5 @@
 use crate::constants::{DEFAULT_CREDIT, DEFAULT_TITLE};
 use crate::document::{FarceDocument, TitlePage};
-use std::fs::File;
 use std::io::Write;
 
 const HTML_HEADER: &[u8] = b"<html><head><style type=\"text/css\">
@@ -15,16 +14,16 @@ div#title-page-credits {text-align: center; margin: 200px auto 200px auto;}
 
 const HTML_FOOTER: &[u8] = b"</body></html>";
 
-fn write_title_page(title_page: &TitlePage, mut f: &File) {
-    f.write(b"<div id=\"title-page\">").unwrap();
+fn write_title_page(title_page: &TitlePage, w: &mut impl Write) {
+    w.write(b"<div id=\"title-page\">").unwrap();
 
-    f.write(b"<div id=\"title-page-credits\">").unwrap();
+    w.write(b"<div id=\"title-page-credits\">").unwrap();
     match title_page.fields.get("Title") {
         Some(title) => {
-            f.write(format!("<p>{}</p>", title).as_bytes()).unwrap();
+            w.write(format!("<p>{}</p>", title).as_bytes()).unwrap();
         }
         None => {
-            f.write(DEFAULT_TITLE.as_bytes()).unwrap();
+            w.write(DEFAULT_TITLE.as_bytes()).unwrap();
         }
     }
 
@@ -32,24 +31,24 @@ fn write_title_page(title_page: &TitlePage, mut f: &File) {
         Some(author) => {
             let default_credit = DEFAULT_CREDIT.to_string();
             let credit = title_page.fields.get("Credit").unwrap_or(&default_credit);
-            f.write(format!("<p>{}</p>", credit).as_bytes()).unwrap();
-            f.write(format!("<p>{}</p>", author).as_bytes()).unwrap();
+            w.write(format!("<p>{}</p>", credit).as_bytes()).unwrap();
+            w.write(format!("<p>{}</p>", author).as_bytes()).unwrap();
         }
         None => (),
     }
-    f.write(b"</div>").unwrap();
+    w.write(b"</div>").unwrap();
 
-    f.write(b"</div>").unwrap();
+    w.write(b"</div>").unwrap();
 }
 
-pub fn write_html(document: FarceDocument, mut f: File) -> Result<(), String> {
-    f.write(HTML_HEADER).unwrap();
+pub fn write_html(document: FarceDocument, mut w: impl Write) -> Result<(), String> {
+    w.write(HTML_HEADER).unwrap();
     if let Some(ref title_page) = document.title_page {
-        write_title_page(title_page, &f);
+        write_title_page(title_page, &mut w);
     }
     for element in &document.elements {
-        f.write(element.as_html().as_bytes()).unwrap();
+        w.write(element.as_html().as_bytes()).unwrap();
     }
-    f.write(HTML_FOOTER).unwrap();
+    w.write(HTML_FOOTER).unwrap();
     Ok(())
 }
